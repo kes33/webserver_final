46c594d41770298d2e1ebc37fa2a3b3db362052c
Kim <kswennen@cs.ucla.edu>
2012-10-09 15:41:02 -0700

    basic socket setup

--- webserver.c
+++ webserver.c
@@ -2,9 +2,30 @@
 //  webserver.c
 //  
 //
-//  Created by Kim Swennen on 10/9/12.
+//  Created by Kim Swennen and Ben Tzou.
 //
 //
 
 #include <stdio.h>
-//test code to test github
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <sys/wait.h>   //do we need this?
+#include <signal.h>     //this too?
+
+int main (int argc, char * argv[]) {
+    
+/*---------socket setup-----------*/
+    int sockfd;     //socket file descriptor
+    
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);   //creating a ipv4 socket to use TCP
+    if (sockfd < 0)
+        perror("error opening socket");
+    
+    
+    
+    
+    
+}
